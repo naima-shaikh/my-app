7b0655e049b0d6e2d0de55a369f84a3d81ea8ab4
ruben profit <rubentprofit@gmail.com>
2019-05-31 16:08:02 -0400

    refactored header
    refactored header component and removed unnecessary divs

--- src/Header.js
+++ src/Header.js
@@ -6,24 +6,20 @@ export default class Header extends Component {
     return (
       <div className="Header">
         <h1> {this.props.name}</h1>
-          <div>
-            <div>
-              <nav className="nav-list">
-                <div className="nav-item">
-                  <Link to="/">Kids</Link>
-                </div>
-                <div className="nav-item">
-                  <Link to="/men">Mens</Link>
-                </div>
-                <div className="nav-item">
-                  <Link to="/women">Womens</Link>
-                </div>
-                <div className="nav-item">
-                  <Link to="/plussize">Plus Size</Link>
-                </div>
-              </nav>
+          <nav className="nav-list">
+            <div className="nav-item">
+              <Link to="/">Kids</Link>
             </div>
-          </div>
+            <div className="nav-item">
+              <Link to="/men">Mens</Link>
+            </div>
+            <div className="nav-item">
+              <Link to="/women">Womens</Link>
+            </div>
+            <div className="nav-item">
+              <Link to="/plussize">Plus Size</Link>
+            </div>
+          </nav>
         </div>
     );
   }
